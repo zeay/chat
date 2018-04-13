a940159bacc5a5f9f7b20ec7c91280402d370611
saurabh <zeayycool@gmail.com>
2018-04-13 20:28:13 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path  = require("path");
+const port = process.env.PORT || 3000;
+const app = express();
+
+ const staticDirName = path.join(__dirname, "../client");
+app.use(express.static(staticDirName))
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`);
+})
